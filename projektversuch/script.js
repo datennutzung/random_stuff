0e582b7a0b4c86e1260f4a4e0bb7e6aa276ddaa5
Miron Gertis <miron@gertis.de>
2021-11-11 12:02:47 +0100

    hopefully fix

--- projektversuch/script.js
+++ projektversuch/script.js
@@ -1,37 +1,37 @@
-const cards = document.querySelectorAll(".card > input")
+const cards = document.querySelectorAll(".card > input");
 
 function toggle_selected() {
-    const cards_checked = document.querySelectorAll(".card > input:checked")
+    const cards_checked = document.querySelectorAll(".card > input:checked");
     for (const card of cards) {
-        card.parentElement.classList.remove("selected")
+        card.parentElement.classList.remove("selected");
     }
     for (const card of cards_checked) {
-        card.parentElement.classList.add("selected")
+        card.parentElement.classList.add("selected");
     }
 }
 
 function update_total() {
     let total = 0.0;
-    const prices = document.querySelectorAll(".card > input:checked ~ .price")
+    const prices = document.querySelectorAll(".card > input:checked ~ .price");
     for (const price of prices) {
-        total += parseInt(price.innerText)+(parseInt(price.children[0].innerText)/100.0);
+        total += parseFloat(price.dataset.price);
     }
-    const total_price = document.querySelector("#total-price")
+    const total_price = document.querySelector("#total-price");
     total_price.innerText = parseInt(total);
-    let total_cent = document.createElement("small")
-    total_cent.classList.add("cent")
-    total_cent.textContent = ((total%1)*100).toString().padStart(2, "0")
-    total_price.appendChild(total_cent)
+    let total_cent = document.createElement("small");
+    total_cent.classList.add("cent");
+    total_cent.textContent = ((total%1)*100).toString().padStart(2, "0");
+    total_price.appendChild(total_cent);
 }
 
 for (const card of cards) {
     card.addEventListener("change", (ev) => {
-        toggle_selected()
-        update_total()
+        toggle_selected();
+        update_total();
     })
 }
 
 
-toggle_selected()
-update_total()    
+toggle_selected();
+update_total();
 
